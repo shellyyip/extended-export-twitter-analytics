210dbd309baafdfd2c5793395902e4239572fbb2
Shelly Njoo <shelly@shellynjoo.com>
2014-04-18 12:19:53 -0400

    Generate button

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,22 @@ var filter = require('../scripts/filter.js');//function
 //https://ads.twitter.com/accounts/xxxxxx/timeline_activity/tweet_data
 $(document).ready(function() {
 	//Get the JSON data
-	var elem = $('.json-input');
-	elem.data('oldVal', elem.val());
-	var input;
+	function generate(tweets,keys) {	
+		keys = getKeys('#tweetkeys','data-prop');
+		console.log(keys);
+		var jsonOutput = filter(tweets, keys);	
+		console.log(jsonOutput);
+		$('.output-display').text(JSON.stringify(jsonOutput));
+		$('.output-display').append(
+			'<h1>'+friendlyNames+'</h1>'
+		);
+		for (var i=0; i < jsonOutput.length; i++) {
+			$('.output-display').append('<p>'+JSON.stringify(jsonOutput[i], null, 4)+'</p><hr>');
+		}		
+	}	
+	//Get keys
 	var keys;
-	var friendlyNames;
+	var friendlyNames;	
 	$('#tweetkeys').find('input[type=checkbox]').bind('click', function() {
 		if ($(this).attr('checked') == 'checked') {
 			$(this).attr('checked',false);
@@ -21,23 +32,21 @@ $(document).ready(function() {
 		keys = getKeys('#tweetkeys','data-prop');
 		console.log(keys);
 	});
-	console.log(keys);
+	//Get tweets
+	var input;
+	var tweets;
+	var elem = $('.json-input');
+	elem.data('oldVal', elem.val());
 	elem.bind('propertychange keyup input paste',
 		function(){
-			input = $('.json-input').val();
+			input = $(this).val();
 			//validate
 			if (input.search('statuses')) {		
-				var tweets = $.parseJSON(input).statuses;	
-				var jsonOutput = filter(tweets, keys);	
-				console.log(jsonOutput);
-				$('.output-display').text(JSON.stringify(jsonOutput));
-				$('.output-display').append(
-					'<h1>'+friendlyNames+'</h1>'
-				);
-				for (var i=0; i < jsonOutput.length; i++) {
-					$('.output-display').append('<p>'+JSON.stringify(jsonOutput[i], null, 4)+'</p><hr>');
-				}
-			}		
+				tweets = $.parseJSON(input).statuses;	
+			}
 		}
 	);
+	$('.generate').bind('click', function() {
+		generate(tweets,keys);
+	});
 });
