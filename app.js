1da2718e2e44d9b12502dffe6fd41c6ecf14ace5
Shelly Njoo <shelly@shellynjoo.com>
2014-04-18 16:00:38 -0400

    More work on simplify tweets &  links

--- app.js
+++ app.js
@@ -112,6 +112,11 @@ var filter = require('../scripts/filter.js');
 // INPUT: array of standard tweet objects (formatted like their API), array of obj of desired properties, optimally mapped as prop:friendlyName
 // OUTPUT: totally simplified tweet objects in array, formatted as friendlyName: propValue
 module.exports = function(rawArray, keysArray){
+	
+	function findDisplayUrl(rawURL, entitiesObj) {
+		return 'boop';
+	}
+	
 	//Save copy of raw array for special cases
 	var originArray = $.extend(true, [], rawArray);	
 	//Get filtered tweet array
@@ -126,9 +131,15 @@ module.exports = function(rawArray, keysArray){
 				//catch all special cases, like links
 				case 'links':
 					var j = 1;
+					var rawEntities = originArray[i].entities;
 					for (linkKey in tweet.links) {
 						//linkKey is minified twitter URL
-						newObj['link '+j] = originArray[i];
+						newObj['*TEST*_'+j] = linkKey;
+						//if media_url == linkKey
+							//return entities.media.display_url
+						//if
+						newObj['*FN-RETURN* '+j] = findDisplayUrl(linkKey, rawEntities)
+						newObj['link '+j] = rawEntities;
 						newObj['click count '+j] = tweet.links[linkKey];
 						j++
 					}
