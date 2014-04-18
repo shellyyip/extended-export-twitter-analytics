e31ea5ce79c2ae4340b4f929b67b16895a4af1fd
Shelly Njoo <shelly@shellynjoo.com>
2014-04-18 15:41:53 -0400

    Initial checkin for special cases in simplify-tweets

--- scripts/simplify-tweets.js
+++ scripts/simplify-tweets.js
@@ -1,21 +1,37 @@
-var getArray = require('../scripts/filter.js');
+var filter = require('../scripts/filter.js');
 // **** SIMPLIFY-TWEETS.JS
+// * Filters raw tweet array to desired keys, then returns a simplified, remapped array of tweets
 // INPUT: array of standard tweet objects (formatted like their API), array of obj of desired properties, optimally mapped as prop:friendlyName
 // OUTPUT: totally simplified tweet objects in array, formatted as friendlyName: propValue
 module.exports = function(rawArray, keysArray){
-	var rawArray = getArray(rawArray, keysArray);
-	return rawArray;
+	//Save copy of raw array for special cases
+	var originArray = $.extend(true, [], rawArray);	
+	//Get filtered tweet array
+	var tweetsArray = filter(rawArray, keysArray);
 	var output = [];
-	//loop through tweets
-	for (var i = 0; i < tweets.length; i++) {
-		switch(n)
-			{
-			case 1:
-			  break;
-			case 2:
-			  break;
-			default:
-				return;
+	//loop through tweets and remap
+	for (var i=0; i < tweetsArray.length; i++) {
+		var newObj = {};
+		var tweet = tweetsArray[i];	
+		for (var key in keysArray) {
+			switch(keysArray[key]) {
+				//catch all special cases, like links
+				case 'links':
+					var j = 1;
+					for (linkKey in tweet.links) {
+						//linkKey is minified twitter URL
+						newObj['link '+j] = originArray[i];
+						newObj['click count '+j] = tweet.links[linkKey];
+						j++
+					}
+					break;
+				default:
+					//regular remap;
+					newObj[keysArray[key]] = tweet[keysArray[key]];
+					
 			}
+		}
+		output.push(newObj);
 	}
+	return output;
 };
